b7a9e08e82558ca25aece04f9a529ecff45b75ff
gaspar-d <ghostlabz@gmail.com>
2020-07-16 00:53:04 -0300

    Added CSS

--- src/components/SearchPage/Products/ProductsPage.jsx
+++ src/components/SearchPage/Products/ProductsPage.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 
+import "./ProductPage.css";
+
+import CardProduct from "./CardProduct/CardProduct";
 import logoUrl from "../../../assets/feed-home/image.jpg";
-import photoUrl from "../../../assets/feed-home/mao-santa-burguer-1531851949973-v-2-900-x-506.png";
 
-/* 
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlRMTGRGN0ZSR1ZCYjZRZks0dDJzIiwibmFtZSI6Ikthc3Bhcm92IiwiZW1haWwiOiJrYXNwYXJvdkBnbWFpbC5jb20iLCJjcGYiOiIxMTEuMTExLjExMS0xMiIsImhhc0FkZHJlc3MiOnRydWUsImFkZHJlc3MiOiJSLiBDaGVzcywgMTcxLCBBIC0gQ2hlZWt5IEJyZWVrIiwiaWF0IjoxNTk0NjgyODQ0fQ.24bewsoHPv3dKl0C8OD5uAPpPCVbQVLNNLlBB9TemoE";
+import { useGetRestaurants } from "../../Hooks/useGetRestaurants";
 
-  const restaurantId = 8;
+const ProductsPage = (props) => {
+  const restaurants = useGetRestaurants();
 
-  const axiosConfig = {
-    headers: {
-      auth: token,
-    },
-  }; */
+  const restaurantPerId = restaurants.filter((restaurant) => {
+    return restaurant.id === "2";
+  });
 
-const ProductsPage = () => {
+  console.log(restaurantPerId);
+
   return (
     <div className="Container">
-      <h1>Produtos aqui</h1>
-      {/* {restaurants.map((restaurant) => (
+      {restaurantPerId.map((restaurant) => (
         <div key={restaurant.id} className="Card">
-          <img src={restaurant.logoUrl} alt="Imagem do card" />
+          <img src={restaurant.logoUrl} alt="Imagem do Produto" />
           <div>
             <p className="Restaurant">{restaurant.name}</p>
+            <p className="ResDescription">{restaurant.description}</p>
             <div className="CardFooter">
               <span className="Time-to-deliver">
                 {restaurant.deliveryTime} - {restaurant.deliveryTime + 20} min
@@ -32,34 +32,12 @@ const ProductsPage = () => {
                 Frete R${restaurant.shipping.toFixed(2)}
               </span>
             </div>
+            <p className="Address">{restaurant.address}</p>
           </div>
         </div>
-      ))} */}
+      ))}
 
-      <div className="Card">
-        <img src={logoUrl} alt="Imagem do Produto" />
-        <div>
-          <p className="Restaurant">Restaurante</p>
-          <p className="Description">Description</p>
-          <div className="CardFooter">
-            <span className="Time-to-deliver">min</span>
-            <span className="Shipping-fee">Frete R$</span>
-          </div>
-          <p className="Address">Address</p>
-        </div>
-      </div>
-
-      <div className="CardProducts">
-        <img className="ImgProduct" src={photoUrl} alt="Imagem do Produto" />
-        <div className="CardAside">
-          <p className="Restaurant">Restaurante</p>
-          <p className="ProductDescription">Description</p>
-          <div className="CardFooterProducts">
-            <span className="Price">R$</span>
-            <button className="ButtonAdd">adicionar</button>
-          </div>
-        </div>
-      </div>
+      <CardProduct />
     </div>
   );
 };

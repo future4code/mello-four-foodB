cd6e206e4cda37c96fcd536f02cdac1073b479a8
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-07-16 21:54:42 -0300

    Component cart: remove product

--- src/components/Cart/index_Cart.js
+++ src/components/Cart/index_Cart.js
@@ -2,17 +2,76 @@ import React, { useState, useEffect } from 'react';
 //import { Link } from 'react-router-dom';
 import * as S from './style_Cart';
 //rgba(232, 34, 46, 0.5);
+//mock carrinho
+//[{"id": 1, "product": "Cart", "price": 15, "image": "https://www.teclasap.com.br/wp-content/uploads/2011/05/hamburger.jpg"}, {"id": 2, "product": "Cart", "price": 13, "image": "https://www.teclasap.com.br/wp-content/uploads/2011/05/hamburger.jpg"}]
 
 const Index_Cart = (props) => {
   const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')));
   const [paymentMethod, setPaymentMethod] = useState('');
   const address = JSON.parse(localStorage.getItem('addressUser'));
-  
+  const [newCart, setNewCart] = useState([]);
+  const auxNewCart = []
+
   const choosePaymentMethod = (event) => {
       setPaymentMethod(event.target.value);
   }
 
-  if (cart.length === 0){
+  useEffect(()  => {
+
+  const listCart = () => {
+    cart.forEach(item => {
+        console.log("item forEach " + JSON.stringify(item))
+        const inArray = auxNewCart.findIndex(index => {
+            return index.id === item.id
+        })
+        console.log(inArray)
+        if(inArray === -1){
+            const prod = {
+                ...item, qtd: 1
+            }
+            auxNewCart.push(prod)
+        } else {
+            auxNewCart.map(product => {
+                if(product.id === item.id){
+                    return {...product, qtd: product.qtd++}
+                }
+                return product
+            })
+        }
+        });
+
+        setNewCart(auxNewCart)
+    }
+
+    if(cart !== null){
+        listCart();
+    }
+
+  },[])
+  console.log(newCart)
+  const removeProduct = (id) => {
+    console.log(newCart)
+    const removeCart = newCart.filter(item => item.id !== id)
+    setNewCart(removeCart);
+    localStorage.removeItem('cart');
+    localStorage.setItem('cart', JSON.parse(newCart))
+  }
+
+  const confirmBuy = () => {
+    const dataBuy = {
+      "products": 
+        [{
+            "id": "CnKdjU6CyKakQDGHzNln",
+            "quantity": 10
+        }, {
+            "quantity": 1,
+            "id": "KJqMl2DxeShkSBevKVre"
+        }],
+        "paymentMethod": "creditcard"
+        }
+  }
+
+  if (newCart.length === 0 || newCart=== undefined){
     return(<S.DivContainer>
         <S.DivSubHeader>Meu carrinho</S.DivSubHeader>
         <S.DivTitleEndereco>
@@ -31,7 +90,7 @@ const Index_Cart = (props) => {
             <S.InputRadio data-testid="money" name="paymentMethod" type="radio" id="money" value="money" onChange={choosePaymentMethod}/> Dinhero
         </div>
         <div>
-            <S.InputRadio data-testid="card" name="paymentMethod" type="radio" id="card" value="card" onChange={choosePaymentMethod}/> Cartão de crédito
+            <S.InputRadio data-testid="card" name="paymentMethod" type="radio" id="card" value="creditcard" onChange={choosePaymentMethod}/> Cartão de crédito
         </div>
         <S.DivDataPayment>
             <S.DivButton>
@@ -48,15 +107,26 @@ const Index_Cart = (props) => {
                 <S.PAddress>{address.address.street}</S.PAddress>
             </S.DivTitleEndereco>
             <S.DivCartFull>
-              {cart.map((product) => {
+              {newCart.length > 0 && newCart.map((product) => {
                 return (
-                  <S.DivCard key={product.id}>{product.product}</S.DivCard>)
+                <S.DivCard key={product.id}>
+                    <S.ImgProduct src={product.image} />
+                    <S.DivProductDetail>
+                        <S.QuantityProducts>{product.qtd}</S.QuantityProducts>
+                        <S.TitleProduct>{product.product}</S.TitleProduct>
+                        <S.DescProduct></S.DescProduct>
+                      <S.BottomCard>  
+                        <S.PriceProduct>{product.price.toFixed(2)}</S.PriceProduct>
+                        <S.ButtonDeleteProduct onClick={() => removeProduct(product.id)}>remover</S.ButtonDeleteProduct>
+                      </S.BottomCard>
+                    </S.DivProductDetail>
+                </S.DivCard>)
             })}
             </S.DivCartFull>
             <S.DivFreight>Frete R$0,00</S.DivFreight>
             <S.DivPayment>
                 <div>SubTotal</div>
-                <div>R${(cart.reduce((acumulador, valor) => acumulador + valor.price, 0)).toFixed(2)}</div>
+                <div>R${(newCart.reduce((acumulador, valor) => acumulador + valor.price, 0)).toFixed(2)}</div>
             </S.DivPayment>
             <S.DivText>Forma de pagamento</S.DivText>
             <hr />
